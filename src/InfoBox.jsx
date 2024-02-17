c29d6e62d4cc7db72f25f2871826e968c88c7ba8
Sahil-Sukhdeve12 <sahilsukhdeve15@gmail.com>
2024-02-17 13:08:29 +0530

    files added

---
+++ src/InfoBox.jsx
@@ -0,0 +1,54 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import "./InfoBox.css";
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+
+export default function InfoBox({info}){
+    const INIT_URL="https://images.unsplash.com/photo-1612297728955-a0ad12a75df9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y2xvdWR5JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    // let info={
+    //     city:"Delhi",
+    //     feelslike:24.84,
+    //     temp:25.05,
+    //     tempMin:25.05,
+    //     tempMax:25.05,
+    //     humidity:47,
+    //     weather:"haze",
+    // };
+
+    let COLD_URL="https://images.unsplash.com/photo-1612208695882-02f2322b7fee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    let HOT_URL="https://images.unsplash.com/photo-1577985759186-0854dfd3f218?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    let RAIN_URL="https://images.unsplash.com/photo-1518803194621-27188ba362c9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHJhaW58ZW58MHx8MHx8fDA%3D";
+
+    return(
+        <div className="InfoBox">
+            {/* <h1>WeatherInfo-{info.weather}</h1> */}
+            <div className="cardContainer">
+            <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={info.humidity>80 ? RAIN_URL : info.temp>15 ? HOT_URL :COLD_URL}
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.city}{
+            info.humidity>80 ? <ThunderstormIcon/> : info.temp>15 ? <WbSunnyIcon/> : <AcUnitIcon/>
+          }
+        </Typography>
+        <Typography variant="body2" color="text.secondary" component={"span"}>
+          <p>Temperature={info.temp}&deg;C</p>
+          <p>Humidity={info.humidity}</p>
+          <p>Min Temp={info.tempMin}&deg;C</p>
+          <p>Max Temp={info.tempMax}&deg;C</p>
+          <p>The weather can be described as <b><i>{info.weather}</i></b> and feels like {info.feelslike}&deg;C</p>
+        </Typography>
+      </CardContent>
+    </Card>
+    </div>
+        </div>
+    )
+}
